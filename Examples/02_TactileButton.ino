6f77ab51e7289b5fe86a598059d67cbef9ce2b37
Gil <gilm2121@gmail.com>
2018-10-06 17:05:50 +0200

    Added examples, 2 approaches to toggling led on button clicks

---
+++ Examples/02_TactileButton.ino
@@ -0,0 +1,35 @@
+// Note: Uncomment next line to set this file as main
+#define TACTILE_BUTTON_IS_MAIN
+#ifdef TACTILE_BUTTON_IS_MAIN
+
+#include "IO/IOComponents.h"
+#include "Componenets/TactileButton.h"
+#include "Componenets/LED.h"
+
+#define BTN_PIN 8
+#define LED_PIN 13
+
+// ** Can also be initialized with DigitalInput:
+// TactileButton button(DigitalInput(BTN_PIN, InputPull::InputPull_Up))
+TactileButton button(BTN_PIN, InputPull::InputPull_Up);
+LED led(LED_PIN);
+
+void setup() {
+	led.off();
+
+	// Register a callback for the onRelease event
+	button.onRelease([&led](){
+		// Led stores its state internally, so it can be toggled
+		led.toggle();
+		// delay to ignore button debounces
+		delay(50);
+	});
+}
+
+void loop() {
+	// we call loop() on button so it can proccess phsical input and call our callbacks
+	button.loop();
+}
+
+
+#endif
---
+++ Examples/03_CustomComponent.ino
@@ -0,0 +1,51 @@
+// Note: Uncomment next line to set this file as main
+//#define CUSTOM_COMPONENT_IS_MAIN
+#ifdef CUSTOM_COMPONENT_IS_MAIN
+
+#include "IO/IOComponents.h"
+#include "Componenets/TactileButton.h"
+#include "Componenets/LED.h"
+
+class ButtonToggledLED : public BaseComponent {
+public:
+	ButtonToggledLED(PinNumber buttonPin, PinNumber ledPin) : 
+		BaseComponent(), 
+		led(ledPin, State::State_Low), 
+		button(buttonPin, InputPull::InputPull_Up) {
+
+		// When we register a components as a child, it's loop is called every time our loop is called
+		// So instead of us needing to call it's loop, it is called automaticaly and the object 
+		// is ready to use and read when execution arrives to this->privateLoop
+		RegisterChild(button);
+		
+		// Register a callback for the onRelease event
+		button.onRelease([&](){
+			// Led stores its state internally, so it can be toggled
+			led.toggle();
+			// delay to ignore button debounces
+			delay(50);
+		});
+	}
+
+private:
+	TactileButton button;
+	LED led;
+
+	void privateLoop() {
+		// By the time this function is called, `button`'s loop had already been executed
+		// and onRelease was called (if the button was actually released), so there is no
+		// work to be done in this function.
+	}
+};
+
+#define BTN_PIN 8
+#define LED_PIN 13
+
+ButtonToggledLED app(BTN_PIN, LED_PIN);
+void setup() {}
+
+void loop() {
+	app.loop();
+}
+
+#endif
