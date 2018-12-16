3cfc3c4e9820c6d58ee2c46853d78c074080a421
Gil Maimon <gilm2121@gmail.com>
2018-12-16 21:16:05 +0200

    added more headers to be included by default when including the library via arduino ide

--- src/ArduinoComponents.h
+++ src/ArduinoComponents.h
@@ -1,1 +1,4 @@
 #include "ArduinoComponents/IO/IOComponents.h"
+#include "ArduinoComponents/Componenets/BaseComponent.h"
+#include "ArduinoComponents/Componenets/LED.h"
+#include "ArduinoComponents/Componenets/TactileButton.h"
