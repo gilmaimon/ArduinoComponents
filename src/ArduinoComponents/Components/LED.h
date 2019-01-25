f8cee5572108cc40f7d3e727fb0eb50f3847a103
Gil Maimon <gilm2121@gmail.com>
2019-01-25 23:14:01 +0200

    added comments for LED

--- src/ArduinoComponents/Components/LED.h
+++ src/ArduinoComponents/Components/LED.h
@@ -5,6 +5,22 @@
 #include "../IO/DigitalOutput.h"
 #include "Component.h"
 
+/*
+Name: LED
+Include: #include <ArduinoComponents/Components/LED.h>
+Sentence: A class for representing a LED, turning it on, off or toggling it. 
+BasicUsage:
+	// For a LED connected on pin 13
+	LED led(13);
+
+	led.on(); // turns the led on
+	led.off(); // turns off the led
+
+	led.toggle(); // switches the led off or on (toggle)
+Notes:
+	- LED has no state "events" so you dont HAVE to call loop, but you should, for uniformity.
+*/
+
 namespace components {
 	class LED : public Component {
 	public:
